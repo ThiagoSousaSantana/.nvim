dd3cdc59d43c08b066b5130e2da941faf6157800
Thiago Santana <thiago91098@gmail.com>
2024-07-23 10:03:48 -0300

    Add go keymappings

--- lua/custom/plugins/gopher-nvim.lua
+++ lua/custom/plugins/gopher-nvim.lua
@@ -3,6 +3,12 @@ return {
   ft = 'go',
   config = function(_, opts)
     require('gopher').setup(opts)
+
+    vim.keymap.set('n', '<leader>cgsj', '<cmd> GoTagAdd json <CR>', { desc = 'Add json struct tags' })
+
+    vim.keymap.set('n', '<leader>cgsy', '<cmd> GoTagAdd yaml <CR>', { desc = 'Add yaml struct tags' })
+
+    vim.keymap.set('n', '<leader>cgie', '<cmd> GoIfErr <CR>', { desc = 'Generate if err' })
   end,
   build = function()
     vim.cmd [[silent! GoInstallDeps]]
