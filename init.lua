2c28616500ba370805622be3a464a559ebbc1443
Thiago Santana <thiago91098@gmail.com>
2024-08-18 19:22:40 -0300

    Add go stuf on mason

--- init.lua
+++ init.lua
@@ -615,6 +615,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'gofumpt',
+        'golines',
+        'goimports-reviser',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
